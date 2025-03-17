414607dd6bc8a056b6e3116724793897180bcf00
Hossein Fazel <fazel.hosein9654@gmail.com>
2025-03-17 13:08:20 +0330

    change client code to accept custom server and port and a name

--- client.py
+++ client.py
@@ -1,22 +1,26 @@
 import socket
+from time import sleep
 
-def start_client():
+def start_client(server, port, name):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        client_socket.connect(('localhost', 8080))
-        print("Connected to server.")
-    except:
-        print("The server is not running")
-        exit(0)
 
-    # Send messages to the server
+    while True:
+        try:
+            client_socket.connect((server, port))
+            print("Your friend is online")
+            break
+        except:
+            print("Your friend is offline")
+            sleep(2)
+
     while True:
         message = input()
         if message.lower() == 'exit':
             break
+        message = f"{name} : " + message
         client_socket.send(message.encode('utf-8'))
 
     client_socket.close()
 
 if __name__ == "__main__":
-    start_client()
+    start_client("localhost", 8080, "test_name")
